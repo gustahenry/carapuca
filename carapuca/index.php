635e4fc8c9ffb184ae510ed8c6e582d6fb7bca16
gustavo.henrique <pcasemirog@gmail.com>
2016-09-13 22:57:49 -0300

    Corpo do HTML e head da pagina

---
+++ carapuca/index.php
@@ -0,0 +1,5 @@
+<?php
+include('head.php');
+include('modulos/destaque.php');
+include('footer.php');
+?>
---
+++ carapuca/modulos/destaque.php
@@ -0,0 +1,31 @@
+<div class="row">
+	<div class="col-md-1"></div>
+
+	<div class="col-md-10">
+		<!- Menu -->
+		<nav class="">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">
+		        <img alt="Brand" src="assests/img/logo.png">
+		      </a>
+		    </div>
+		    <ul class="	nav navbar-nav">
+		    <li><a href="#">Serviços</a></li>
+		    <li><a href="#">Portifolio</a></li>
+		    <li><a href="#">Sobre Nòs</a></li>
+		    <li><a href="#">Contato</a></li>
+		    </ul>
+		  </div>
+		</nav>
+
+		<!- Destaque -->
+		<div class="jumbotron">
+		  <h1></h1>
+		  <p></p>
+		  <p><a class="btn btn-primary btn-lg" href="#" role="button">Confira</a></p>
+		</div>
+	</div>
+	<div class="col-md-1"></div>
+
+</div>
